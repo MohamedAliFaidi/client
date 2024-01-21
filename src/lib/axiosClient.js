6d638c14312b7911ccf9a7b6f0a7bf5387648164
MohamedAliFaidi <dalyyy3259@protonmail.com>
2024-01-21 19:39:23 +0100

    progress

--- src/lib/axiosClient.js
+++ src/lib/axiosClient.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 
 export const axiosClient = axios.create({
+   baseURL:"https://webpack-mern.vercel.app",
     timeout: 1000 * 60 * 5,
     withCredentials: true,
     headers: {
--- vite.config.js
+++ vite.config.js
@@ -6,35 +6,33 @@ import viteCompression from 'vite-plugin-compression';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),viteCompression()],
-  server: {
-    proxy: {
-      // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+  // server: {
+  //   proxy: {
+  //     // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
     
-      // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
-      '/api': {
-        target: "https://webpack-mern.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      // with RegEx: http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
-    //   '^/fallback/.*': {
-    //     target: 'http://jsonplaceholder.typicode.com',
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/fallback/, ''),
-    //   },
-    //   // Using the proxy instance
-      // '/api': {
-      //   target: 'https://webpack-mern.vercel.app',
-      //   changeOrigin: true,
-      //   configure: (proxy, options) => {
-      //     // proxy will be an instance of 'http-proxy'
-      //   },
-      // },
-    //   // Proxying websockets or socket.io: ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
-    //   '/socket.io': {
-    //     target: 'ws://localhost:5174',
-    //     ws: true,
-    //   },
-    },
-  },
+  //     // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+  //     // '/api': {
+  //     //   target: "https://webpack-mern.vercel.app",
+  //     //   changeOrigin: false,
+  //     //   rewrite: (path) => path.replace(/^\/api/, ''),
+  //     // },
+  //     // with RegEx: http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
+  //   //   '^/fallback/.*': {
+  //   //     target: 'http://jsonplaceholder.typicode.com',
+  //   //     changeOrigin: true,
+  //   //     rewrite: (path) => path.replace(/^\/fallback/, ''),
+  //   //   },
+  //     // Using the proxy instance
+  //     // '/api': {
+  //     //   target: 'https://webpack-mern.vercel.app',
+  //     //   changeOrigin: false,
+      
+  //     // },
+  //   //   // Proxying websockets or socket.io: ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
+  //   //   '/socket.io': {
+  //   //     target: 'ws://localhost:5174',
+  //   //     ws: true,
+  //   //   },
+   
+
 })
